7ac3e4d51aad88ed70f38e84df687849ee169c42
farrert <farrert@mail.nmc.edu>
2021-11-23 14:33:28 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ReactApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
